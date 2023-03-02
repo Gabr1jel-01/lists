d7355b34bf5e402608976c9a3b99054d46e99a65
Gabrijel <g.gordic@outlook.com>
2023-03-02 19:55:50 +0100

    Create lists_07.py

---
+++ lists_07.py
@@ -0,0 +1,16 @@
+#numbers = []
+
+#for i in range(100):
+   # numbers.append(i)
+
+numbers = list(range(100))
+
+first_element = numbers[0]
+last_element = numbers[-1]
+
+print()
+print(numbers)
+print()
+print(first_element)
+print(last_element)
+print()
